3c350c0e8f891716ae54988073f0b838ca0d9ff9
Derek Wang <birdinforest@gmail.com>
2026-01-14 23:17:42 +1100

    dev: update dev-run script.

--- dev-run.sh
+++ dev-run.sh
@@ -7,7 +7,7 @@ set -e
 
 SCRIPT_DIR="$(dirname "$0")"
 PROJECT_ROOT="."
-SETTINGS_PATH="/Users/forrrest/projects/UO-BritainKnights/settings.json"
+SETTINGS_PATH="/Users/forrrest/projects/UO-BritainKnights/Game/settings.json"
 # SETTINGS_PATH="/Users/forrrest/projects/OpenUO/settings/settings_osi.json"
 # SETTINGS_PATH="/Users/forrrest/projects/UO-Adventures-Dev/ClassicUO/settings.json"
 
